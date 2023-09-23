e6517c0d748841f47b05385c231cf041bc683ba2
Pipedsl <h.navarrete@duocuc.cl>
2023-09-23 10:05:31 -0300

    commit 4º se agrega carpeta te

---
+++ src/app/lista-participantes/types/IParticipantes.ts
@@ -0,0 +1,12 @@
+export interface IParticipantes {
+    ID: number;
+    foto: string;
+    Nombre: string;
+    Apellido: string;
+    Descripción: string;
+    Actividad: string;
+    Hora_entrada: string;
+    Hora_salida: string;
+    Clasificación: string;
+    Estado: "Activo" | "Inactivo";
+}
